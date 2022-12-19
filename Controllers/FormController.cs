18852cda77469c551c32dfb6f079d38bf558c288
buseclk12 <105548367+buseclk12@users.noreply.github.com>
2022-12-19 18:15:22 +0300

    Add files via upload

---
+++ Controllers/FormController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class FormController : Controller
+    {
+        // GET: Form
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(FormCollection form)
+        {
+            formEntities db = new formEntities();
+            User model = new User();
+            model.Name = form["firstname"].Trim();
+            model.Surname = form["lastname"].Trim();
+            model.Identity = form["identity"].Trim();
+            model.Email = form["email"].Trim();
+            model.Phone = form["cardnumber"].Trim();
+            model.City = form["city"].Trim();
+            model.Birthday = form["birthday"].Trim();
+            model.Photo = form["dosya"].Trim();
+            model.Faal = form["faaliyet"].Trim();
+            model.Opinion = form["subject"].Trim();
+            db.User.Add(model);
+            db.SaveChanges();
+            return View();
+        }
+        public ActionResult Delete(String id)
+        {
+           
+            formEntities db = new formEntities();
+            if (String.IsNullOrEmpty(id))
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User userInfo = db.User.Find(id);
+            if (userInfo.Identity == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userInfo);
+        }
+        [HttpPost]
+        [ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(String id)
+        {
+          
+            formEntities db = new formEntities();
+            User userInfo = db.User.Find(id);
+            db.User.Remove(userInfo);
+            db.SaveChanges();
+            return RedirectToAction("/Index");
+        }
+
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+       // [Route("/User/{id:String}")]
+        public ActionResult Index()
+        {
+            using (var context = new formEntities())
+            {
+                var userInfo = context.User.ToList();
+                return View(userInfo);
+            }
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
